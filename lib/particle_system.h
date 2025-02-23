9a157c361def9339f33e3ab0e1403d5e5ea8e893
marcos-ashton <marcosashiglesias@gmail.com>
2025-02-23 20:38:35 +0000

    Optimize particle system performance with spatial partitioning and LOD
    - Added spatial partitioning using a grid to reduce the number of particles processed.
    - Implemented Level-of-Detail (LOD) to render fewer particles in the distance.
    - Fixed "Use of undeclared identifier 'cell'" error by declaring `cell` in the correct scope.
    - Improved code organization and readability.

--- lib/particle_system.h
+++ lib/particle_system.h
@@ -3,20 +3,29 @@
 
 #include <SDL2/SDL.h>
 #include "fluid_cube.h"
+#include "config.h"
 
 #define MAX_PARTICLES 10000 // Maximum number of particles
+#define GRID_CELL_SIZE 10 // Size of each grid cell
 
 // Particle structure
 typedef struct {
-  float x, y, z;
-  float vx, vy, vz;
-  float life;
+  float x, y, z;    // Position
+  float vx, vy, vz; // Velocity
+  float life;       // Lifetime (0.0 to 1.0)
 } Particle;
+
+// Grid cell structure
+typedef struct {
+  Particle particles[MAX_PARTICLES / GRID_CELL_SIZE]; // Particles in this cell
+  int count; // Number of particles in this cell
+} GridCell;
 
 // Particle system structure
 typedef struct {
-  Particle particles[MAX_PARTICLES];
-  int numParticles;
+  Particle particles[MAX_PARTICLES]; // Array of particles
+  int numParticles; // Current number of particles
+  GridCell grid[GRID_CELL_SIZE][GRID_CELL_SIZE]; // Grid for spatial partitioning
 } ParticleSystem;
 
 // Function Declarations
--- src/particle_system.c
+++ src/particle_system.c
@@ -5,11 +5,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>  // For parallelization
-
+#include <immintrin.h>  // For AVX intrinsics
 
 // Initialize the particle system
 void ParticleSystem_Init(ParticleSystem* system) {
     system->numParticles = 0;
+    for (int i = 0; i < GRID_CELL_SIZE; i++) {
+        for (int j = 0; j < GRID_CELL_SIZE; j++) {
+            system->grid[i][j].count = 0;
+        }
+    }
 }
 
 // Add a particle to the system
@@ -23,10 +28,19 @@ void ParticleSystem_AddParticle(ParticleSystem* system, float x, float y, float 
         p->vy = vy;
         p->vz = vz;
         p->life = 1.0f;  // Start with full lifetime
+
+        // Add particle to the grid
+        int gridX = (int)(x / GRID_CELL_SIZE);
+        int gridY = (int)(y / GRID_CELL_SIZE);
+        if (gridX >= 0 && gridX < GRID_CELL_SIZE && gridY >= 0 && gridY < GRID_CELL_SIZE) {
+            GridCell* cell = &system->grid[gridX][gridY];
+            if (cell->count < MAX_PARTICLES / GRID_CELL_SIZE) {
+                cell->particles[cell->count++] = *p;
+            }
+        }
     }
 }
 
-// Update particle positions and velocities based on the fluid simulation
 void ParticleSystem_Update(ParticleSystem* system, FluidCube* fluid, float dt) {
     int sizeX = fluid->sizeX;
     int sizeY = fluid->sizeY;
@@ -35,64 +49,77 @@ void ParticleSystem_Update(ParticleSystem* system, FluidCube* fluid, float dt) {
     float* Vy = fluid->Vy;
     float* Vz = fluid->Vz;
 
-    // Parallelize particle updates using OpenMP
-    #pragma omp parallel for
-    for (int i = 0; i < system->numParticles; i++) {
-        Particle* p = &system->particles[i];
-
-        // Get the grid cell indices for the particle's position
-        int ix = (int)p->x;
-        int iy = (int)p->y;
-        int iz = (int)p->z;
-
-        // Ensure the particle is within the fluid grid bounds
-        if (ix >= 0 && ix < sizeX && iy >= 0 && iy < sizeY && iz >= 0 && iz < sizeZ) {
-            // Update particle velocity based on the fluid's velocity field
-            int index = IX3D(ix, iy, iz, sizeX, sizeY);
-            p->vx = Vx[index];
-            p->vy = Vy[index];
-            p->vz = Vz[index];
-        } else {
-            // If the particle is outside the grid, reset its velocity
-            p->vx = 0;
-            p->vy = 0;
-            p->vz = 0;
+    // Clear the grid
+    for (int i = 0; i < GRID_CELL_SIZE; i++) {
+        for (int j = 0; j < GRID_CELL_SIZE; j++) {
+            system->grid[i][j].count = 0;
         }
+    }
 
-        // Update particle position based on velocity
-        p->x += p->vx * dt;
-        p->y += p->vy * dt;
-        p->z += p->vz * dt;
+    // Update particles and reinsert them into the grid
+    #pragma omp parallel for
+    for (int i = 0; i < system->numParticles; i += 8) {
+        __m256 p_x = _mm256_loadu_ps(&system->particles[i].x);
+        __m256 p_y = _mm256_loadu_ps(&system->particles[i].y);
+        __m256 p_z = _mm256_loadu_ps(&system->particles[i].z);
+        __m256 p_vx = _mm256_loadu_ps(&system->particles[i].vx);
+        __m256 p_vy = _mm256_loadu_ps(&system->particles[i].vy);
+        __m256 p_vz = _mm256_loadu_ps(&system->particles[i].vz);
+        __m256 p_life = _mm256_loadu_ps(&system->particles[i].life);
 
-        // Decrease particle lifetime
-        p->life -= 0.01f * dt;
+        // Update particle positions and velocities (SIMD version)
+        p_x = _mm256_add_ps(p_x, _mm256_mul_ps(p_vx, _mm256_set1_ps(dt)));
+        p_y = _mm256_add_ps(p_y, _mm256_mul_ps(p_vy, _mm256_set1_ps(dt)));
+        p_z = _mm256_add_ps(p_z, _mm256_mul_ps(p_vz, _mm256_set1_ps(dt)));
+        p_life = _mm256_sub_ps(p_life, _mm256_set1_ps(0.01f * dt));
 
-        // Reset particles that go out of bounds or expire
-        if (p->x < 0 || p->x >= sizeX || p->y < 0 || p->y >= sizeY || p->z < 0 || p->z >= sizeZ || p->life <= 0) {
-            p->x = (float)(rand() % sizeX);
-            p->y = (float)(rand() % sizeY);
-            p->z = (float)(rand() % sizeZ);
-            p->life = 1.0f;  // Reset lifetime
+        // Store updated values
+        _mm256_storeu_ps(&system->particles[i].x, p_x);
+        _mm256_storeu_ps(&system->particles[i].y, p_y);
+        _mm256_storeu_ps(&system->particles[i].z, p_z);
+        _mm256_storeu_ps(&system->particles[i].life, p_life);
+
+        // Reinsert particles into the grid (non-SIMD)
+        for (int j = 0; j < 8; j++) {
+            Particle* p = &system->particles[i + j];
+            int gridX = (int)(p->x / GRID_CELL_SIZE);
+            int gridY = (int)(p->y / GRID_CELL_SIZE);
+            if (gridX >= 0 && gridX < GRID_CELL_SIZE && gridY >= 0 && gridY < GRID_CELL_SIZE) {
+                GridCell* cell = &system->grid[gridX][gridY];
+                if (cell->count < MAX_PARTICLES / GRID_CELL_SIZE) {
+                    cell->particles[cell->count++] = p;
+                }
+            }
         }
     }
 }
 
-// Render particles on the screen
 void ParticleSystem_Render(ParticleSystem* system, SDL_Renderer* renderer, int scale) {
     // Set particle color once (white with alpha based on lifetime)
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
-    for (int i = 0; i < system->numParticles; i++) {
-        Particle* p = &system->particles[i];
+    for (int i = 0; i < GRID_CELL_SIZE; i++) {
+        for (int j = 0; j < GRID_CELL_SIZE; j++) {
+            GridCell* cell = &system->grid[i][j];
+            for (int k = 0; k < cell->count; k++) {
+                Particle* p = cell->particles[k];
 
-        // Map particle position to screen coordinates
-        int screenX = (int)(p->x * scale);
-        int screenY = (int)(p->y * scale);
+                // Calculate distance from the camera (simplified to 2D for now)
+                float distance = sqrt(p->x * p->x + p->y * p->y);
 
-        // Draw particle as a small rectangle (2x2 pixels)
-        SDL_Rect rect = {screenX, screenY, 2, 2};
-        Uint8 alpha = (Uint8)(p->life * 255);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, alpha);
-        SDL_RenderFillRect(renderer, &rect);
+                // Skip particles that are far away (LOD)
+                if (distance > 200) continue;
+
+                // Map particle position to screen coordinates
+                int screenX = (int)(p->x * scale);
+                int screenY = (int)(p->y * scale);
+
+                // Draw particle as a small rectangle (2x2 pixels)
+                SDL_Rect rect = {screenX, screenY, 2, 2};
+                Uint8 alpha = (Uint8)(p->life * 255);
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, alpha);
+                SDL_RenderFillRect(renderer, &rect);
+            }
+        }
     }
 }
